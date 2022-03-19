5d04e20f3416a1c9d151f6812f8977e9dc33fd4a
Liam Desmond <liamjdesmond84@gmail.com>
2022-03-19 18:47:43 -0500

    initial commit

---
+++ src/test/java/com/liam/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liam.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
